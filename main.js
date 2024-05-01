cb53a356a0ea37dfef51517f3680db23fbb17c9f
Moon Byte <34653601+m00nbyt3@users.noreply.github.com>
2024-05-01 14:06:21 +0200

    prevented infinite loop
    There was a while(true) loop that preventing the user from exiting the prompt if the prompt kept being incorrect.
    The loop has been removed so now whenever the prompt is not correct, the user should click on the BH date again

--- main.js
+++ main.js
@@ -18,21 +18,14 @@ function wnalert()
 	var daysSinceFriday = (dayOfWeek + 7 - 5) % 7;
 
 	//Calculate how much days we have to count
-	while (true)
+	var semana = prompt("En que semana de Whitenova estas? 1 o 2");
+	if (Number(semana) == 2)
+		daysSinceFriday += 7;
+	else if (Number(semana) != 1)
 	{
-		var semana = prompt("En que semana de Whitenova estas? 1 o 2");
-		if (Number(semana) == 1)
-			break;
-		else if (Number(semana) == 2)
-		{
-			daysSinceFriday += 7;
-			break;
-		}
-		else
-			alert("Dato invalido, introduce 1 o 2");
-		
+		alert("Dato invalido, introduce 1 o 2");
+		return;
 	}
-	//console.log(daysSinceFriday);
 
 	//Calculate the hours and minutes left to complete wn
 	var tot = 0;
@@ -63,13 +56,13 @@ function wnalert()
 async function main()
 {
 	await sleep(1000);
+
 	console.log("WNL --> Preparado para su uso! Haz click encima de la fecha del Black Hole para ver cuanto tiempo te falta para entrar en el White Nova.");
 	console.log("WNL --> ATENCION: Esta herramienta no es oficial y su uso es puramente informativo, pero no debe de usarse para reclamar o como prueba.");
 	console.log("WNL --> Esta herramienta no tiene en cuenta las horas realizadas durante el viernes, antes del cierre. Esta herramienta toma todas las horas del viernes\
 	como pertenecientes al siguiente ciclo. Esta hecho asi porque no existe una hora oficial, y porque al desarrollador no le gusta madrugar y siempre llega\
 	despues de la hora de cierre.");
-	//Older element clicker
-	//torun = document.getElementsByClassName("end-goal")[0];
+
 	torun = document.getElementById("blackhole-tab");
 	torun.addEventListener("click", wnalert);
 }
