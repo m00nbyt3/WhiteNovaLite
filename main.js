c2bc1048c2b0d1d86cd36da7c8d81d7661258205
Moon Byte <34653601+m00nbyt3@users.noreply.github.com>
2024-05-01 13:26:24 +0200

    Arreglos generales en main.js
    - Actualizados y adaptados los elementos a la web actual
    - Nuevo sistema de calculo de dias, dependiendo de el usuario para saber en que semana esta
    - Logs en consola actualizados

--- main.js
+++ main.js
@@ -4,40 +4,75 @@ function sleep(ms) {
 
 function wnalert()
 {
+	//Selects the hours and days html table and set pos in the last day
 	var time = document.querySelectorAll("[data-original-title]");
 	var pos = time.length - 1;
-	const start = new Date("2023-01-13");
-	const end = start + (86400000 * 14);
-	var day = Date.now();
+
+	// Get actual date
+	var today = new Date();
+
+	// Get week day (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+	var dayOfWeek = today.getDay();
+
+	// Calculate how much days have passed from last friday
+	var daysSinceFriday = (dayOfWeek + 7 - 5) % 7;
+
+	//Calculate how much days we have to count
+	while (true)
+	{
+		var semana = prompt("En que semana de Whitenova estas? 1 o 2");
+		if (Number(semana) == 1)
+			break;
+		else if (Number(semana) == 2)
+		{
+			daysSinceFriday += 7;
+			break;
+		}
+		else
+			alert("Dato invalido, introduce 1 o 2");
+		
+	}
+	//console.log(daysSinceFriday);
+
+	//Calculate the hours and minutes left to complete wn
 	var tot = 0;
 	var hours = 0;
 	var mins = 0;
-	while (day >= start)
+	var day = 0;
+	while (day < daysSinceFriday)
 	{
 		hours = time[pos].getAttribute('data-original-title');
 		tot += (hours[0] * 60);
 		mins = hours[2].concat(hours[3]);
 		tot = parseInt(tot) + parseInt(mins);
-		day -= 86400000; 
+		day++;
 		pos--;
 	}
 	var per = (tot / 60) * 100 / 12;
-	console.log(tot / 60);
-	console.log(per);
+	//console.log(tot / 60);
+	//console.log(per);
 	var left = 720 - tot;
 	if (left < 0)
-		alert("White Nova complete!!!");
+		alert("White Nova completado!!!");
 	else
 	{
-		alert((left / 60).toFixed(0) + " hours and "+ ((((left/60) % 1).toFixed(2)) * 60).toFixed(0) + " minutes left (" + per.toFixed(2) + "%)");
+		alert("Faltan " + (left / 60).toFixed(0) + " horas y "+ ((((left/60) % 1).toFixed(2)) * 60).toFixed(0) + " minutos (" + per.toFixed(2) + "%)");
 	}
 }
 
+
+//Assures all the web elements have finished loading
 async function main()
 {
 	await sleep(1000);
-	console.log("WNL loaded!")
-	torun = document.getElementsByClassName("tab-content goal-container-infos")[0];
+	console.log("WNL --> Preparado para su uso! Haz click encima de la fecha del Black Hole para ver cuanto tiempo te falta para entrar en el White Nova.");
+	console.log("WNL --> ATENCION: Esta herramienta no es oficial y su uso es puramente informativo, pero no debe de usarse para reclamar o como prueba.");
+	console.log("WNL --> Esta herramienta no tiene en cuenta las horas realizadas durante el viernes, antes del cierre. Esta herramienta toma todas las horas del viernes\
+	como pertenecientes al siguiente ciclo. Esta hecho asi porque no existe una hora oficial, y porque al desarrollador no le gusta madrugar y siempre llega\
+	despues de la hora de cierre.");
+	//Older element clicker
+	//torun = document.getElementsByClassName("end-goal")[0];
+	torun = document.getElementById("blackhole-tab");
 	torun.addEventListener("click", wnalert);
 }
 
